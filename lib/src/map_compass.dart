a9f39fe97a0fb56283eb8cefd8fcb2f31993fc69
codscino <nonmerecordo.coscino@gmail.com>
2024-09-05 22:59:07 +0200

    try longpres reset

--- lib/src/map_compass.dart
+++ lib/src/map_compass.dart
@@ -107,7 +107,11 @@ class _MapCompassState extends State<MapCompass> with TickerProviderStateMixin {
           onTap: widget.onPressed ?? () => _resetRotation(context, camera),
           onLongPressStart: (_) {
             _longPressTimer = Timer(const Duration(milliseconds: 500), () {
-              widget.onLongPress?.call();
+              if (widget.onLongPress != null) {
+                widget.onLongPress!.call();
+              } else {
+                _resetRotation(context, camera);
+              }
             });
           },
           onLongPressEnd: (_) {
